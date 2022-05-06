5844df0862541773716535b9dbe4cf819c2c96ed
sat <satoru.ogura@me.com>
2022-05-06 09:31:20 +0000

    64CPU

--- cpu.cpp
+++ cpu.cpp
@@ -10,6 +10,14 @@ using namespace chrono;
 
 #include	<immintrin.h>
 
+void
+DumpM512( __m512 _ ) {
+	cerr << _[  0 ] << ',' << _[  1 ] << ',' << _[  2 ] << ',' << _[  3 ] << endl;
+	cerr << _[  4 ] << ',' << _[  5 ] << ',' << _[  6 ] << ',' << _[  7 ] << endl;
+	cerr << _[  8 ] << ',' << _[  9 ] << ',' << _[ 10 ] << ',' << _[ 11 ] << endl;
+	cerr << _[ 12 ] << ',' << _[ 13 ] << ',' << _[ 14 ] << ',' << _[ 15 ] << endl;
+}
+
 #define	M	512
 #define	K	1024
 #define	N	2048
@@ -25,7 +33,44 @@ MatPro( float* a, float* b, float* c ) {
 	}
 }
 
-/*	NEED MAJOR CONVERSION
+struct
+Args {
+	float*	a;
+	float*	b;
+	float*	c;
+};
+
+#define	NUM_THREADS	64
+void*
+SubMatPro( void* _ ) {
+	float* a = ((Args*)_)->a;
+	float* b = ((Args*)_)->b;
+	float* c = ((Args*)_)->c;
+	for ( auto m = 0; m < ( M / NUM_THREADS ); m++ ) {
+		for ( auto n = 0; n < N; n++ ) {
+			auto $ = 0.;
+			for ( auto k = 0; k < K; k++ ) $ += a[ m * K + k ] * b[ k * N + n ];
+			c[ m * N + n ] = $;
+		}
+	}
+	pthread_exit( NULL );
+}
+
+void
+MatProMulti( float* a, float* b, float* c ) {
+	pthread_t	p[ NUM_THREADS ];
+	Args		args[ NUM_THREADS ];
+	for ( auto _ = 0; _ < NUM_THREADS; _++ ) {
+		args[ _ ].a = a + K * ( M / NUM_THREADS ) * _;
+		args[ _ ].b = b;
+		args[ _ ].c = c + N * ( M / NUM_THREADS ) * _;
+		pthread_create( &p[ _ ], NULL, SubMatPro, args + _ );
+	}
+	for ( auto _ = 0; _ < NUM_THREADS; _++ ) {
+		pthread_join( p[ _ ], NULL );
+	}
+}
+
 void
 MatPro512( float* a, float* b, float* c ) {
 	for ( auto m = 0; m < M; m++ ) {
@@ -44,6 +89,46 @@ MatPro512( float* a, float* b, float* c ) {
 	}
 }
 
+#undef	NUM_THREADS
+#define	NUM_THREADS	32
+void*
+SubMatPro512( void* _ ) {
+	float* a = ((Args*)_)->a;
+	float* b = ((Args*)_)->b;
+	float* c = ((Args*)_)->c;
+	for ( auto m = 0; m < ( M / NUM_THREADS ); m++ ) {
+		auto cPtr = c + m * N;
+		for ( auto n = 0; n < N; n++ ) {
+			auto $ = _mm512_setzero_ps();
+			for ( auto k = 0; k < K; k += 16 ) {
+				$ = _mm512_fmadd_ps(
+					_mm512_load_ps( a + m * K + k )
+				,	_mm512_load_ps( b + n * K + k )
+				,	$
+				);
+			}
+			cPtr[ n ] = _mm512_reduce_add_ps( $ );
+		}
+	}
+	pthread_exit( NULL );
+}
+
+void
+MatPro512Multi( float* a, float* b, float* c ) {
+	pthread_t	p[ NUM_THREADS ];
+	Args		args[ NUM_THREADS ];
+	for ( auto _ = 0; _ < NUM_THREADS; _++ ) {
+		args[ _ ].a = a + K * ( M / NUM_THREADS ) * _;
+		args[ _ ].b = b;
+		args[ _ ].c = c + N * ( M / NUM_THREADS ) * _;
+		pthread_create( &p[ _ ], NULL, SubMatPro512, args + _ );
+	}
+	for ( auto _ = 0; _ < NUM_THREADS; _++ ) {
+		pthread_join( p[ _ ], NULL );
+	}
+}
+
+/*
 void
 MatPro512Cached( float* a, float* b, float* c ) {
 	__m512 m512s[ K / 16 ];
@@ -67,24 +152,6 @@ MatPro512Cached( float* a, float* b, float* c ) {
 	}
 }
 */
-
-//	NEED BLOCK CONVERSION
-void
-MatPro512GEMM( float* a, float* b, float* c ) {
-	for ( auto m = 0; m < M; m += 4 ) {
-		for ( auto n = 0; n < N; n += 4 ) {
-			auto $ = _mm512_setzero_ps();
-			for ( auto k = 0; k < K; k += 4 ) {
-				$ = _mm512_fmadd_ps(
-					_mm512_load_ps( a + m * K + k * 4 )
-				,	_mm512_load_ps( b + k * N + n * 4 )
-				,	$
-				);
-			}
-			_mm512_store_ps( c + m * N + n * 4, $ );
-		}
-	}
-}
 
 void
 Check( float* p, float* q ) {
@@ -113,73 +180,35 @@ Main() {
 		cerr << duration_cast<nanoseconds>( system_clock::now() - start ).count() << " ns" << endl;
 	}
 
-	{	//	BLOCK CONVERSION
-		auto tmp = new ( align_val_t{ 64 } ) float[ M * K ]();	//	Zero cleared
-		auto _ = 0;
-		for ( auto m = 0; m < M; m += 4 ) {
-			for ( auto k = 0; k < K; k += 4 ) {
-				auto $ = a + m * K + k;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += K;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += K;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += K;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-			}
-		}
-		delete[] a;
-		a = tmp;
+	{	auto start = system_clock::now();
+		MatProMulti( a, b, c );
+		cerr << duration_cast<nanoseconds>( system_clock::now() - start ).count() << " ns" << endl;
+		Check( c, ANS );
 	}
 
-//for ( auto _ = 0; _ < 1024; ) {
-//	cerr << ' ' << a[ _ ];
-//	if ( ++_ % 16 == 0 ) cerr << endl;
-//}
-
-	{	//	BLOCK CONVERSION
-		auto tmp = new ( align_val_t{ 64 } ) float[ K * N ]();	//	Zero cleared
+	{	//	B BLOCK CONVERSION
+		auto tmp = new ( align_val_t{ 64 } ) float[ K * N ];
 		auto _ = 0;
-		for ( auto k = 0; k < K; k += 4 ) {
-			for ( auto n = 0; n < N; n += 4 ) {
-				auto $ = b + k * N + n;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += N;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += N;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += N;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
+		for ( auto n = 0; n < N; n++ ) {
+			for ( auto k = 0; k < K; k++ ) {
+				tmp[ _++ ] = b[ n + k * N ]; 
 			}
 		}
 		delete[] b;
 		b = tmp;
+cerr << hex << long( b ) << " - " << long( b + K * N ) << dec << endl;
 	}
 
 	{	auto start = system_clock::now();
-		MatPro512GEMM( a, b, c );
+		MatPro512( a, b, c );
 		cerr << duration_cast<nanoseconds>( system_clock::now() - start ).count() << " ns" << endl;
+		Check( c, ANS );
 	}
-	{	//	BLOCK REVERSE CONVERSION
-		auto tmp = new ( align_val_t{ 64 } ) float[ K * N ]();	//	Zero cleared
-		auto _ = 0;
-		for ( auto k = 0; k < K; k += 4 ) {
-			for ( auto n = 0; n < N; n += 4 ) {
-				auto $ = b + k * N + n;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += N;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += N;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-				$ += N;
-				tmp[ _++ ] = *$; tmp[ _++ ] = *( $ + 1 ); tmp[ _++ ] = *( $ + 2 ); tmp[ _++ ] = *( $ + 3 );
-			}
-		}
-		delete[] b;
-		b = tmp;
+	{	auto start = system_clock::now();
+		MatPro512Multi( a, b, c );
+		cerr << duration_cast<nanoseconds>( system_clock::now() - start ).count() << " ns" << endl;
+		Check( c, ANS );
 	}
-
-	Check( c, ANS );
 
 	delete[] a;
 	delete[] b;
@@ -188,6 +217,10 @@ Main() {
 
 int
 main() {
-	Main();
+	try {
+		Main();
+	} catch ( const char* _ ) {
+		cerr << _ << endl;
+	}
 }
 
