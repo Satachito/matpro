89f9541c35193e82df0bbad67e464d50b2806a25
sat <satoru.ogura@me.com>
2022-05-10 13:56:36 +0000

    Turing

--- tc.sh
+++ tc.sh
@@ -2,6 +2,8 @@ nvcc	\
 	-DCUDA_MANAGED	\
 	-arch=sm_86	\
 	--std=c++17	\
+	-gencode arch=compute_70,code=sm_70	\
+	-gencode arch=compute_75,code=sm_75	\
 	-gencode arch=compute_80,code=sm_80	\
 	-gencode arch=compute_86,code=compute_86	\
 	tc.cu
@@ -13,6 +15,8 @@ nvcc	\
 	-DCUDA_LEGACY	\
 	-arch=sm_86	\
 	--std=c++17	\
+	-gencode arch=compute_70,code=sm_70	\
+	-gencode arch=compute_75,code=sm_75	\
 	-gencode arch=compute_80,code=sm_80	\
 	-gencode arch=compute_86,code=compute_86	\
 	tc.cu
